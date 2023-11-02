42104411371faa48eccf4a6867613e14188139f5
Ilnur Garipov <garipov.i@triadasite.ru>
2023-11-02 22:02:14 +0300

    task/006-solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val changeButton: Button = findViewById(R.id.changeButton)
+        val titleTextView: TextView = findViewById(R.id.titleTextView)
+        changeButton.setOnClickListener {
+            titleTextView.text = "I am an Android Developer!"
+        }
     }
 }
