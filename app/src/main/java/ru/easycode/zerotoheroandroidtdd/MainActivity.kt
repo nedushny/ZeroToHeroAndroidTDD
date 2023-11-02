1abedbbec2e7253966bfe17cbc4872e3d334e8fa
Ilnur Garipov <garipov.i@triadasite.ru>
2023-11-02 22:32:40 +0300

    task/009-solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,32 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val titleTextView: TextView? = findViewById(R.id.titleTextView)
+        val rootLayout: LinearLayout = findViewById(R.id.rootLayout)
+        savedInstanceState?.let {
+            if (!it.getBoolean("isTitleTextViewExist"))
+                rootLayout.removeView(titleTextView)
+        }
+        val button: Button = findViewById(R.id.removeButton)
+        button.setOnClickListener {
+            rootLayout.removeView(titleTextView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(
+            "isTitleTextViewExist",
+            findViewById<TextView>(R.id.titleTextView) != null
+        )
     }
 }
