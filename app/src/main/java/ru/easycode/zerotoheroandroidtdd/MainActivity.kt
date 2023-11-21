398f20615302a243a507314a7deeb3f21b52a349
Ilnur Garipov <garipov.i@triadasite.ru>
2023-11-21 11:37:34 +0300

    task/022-solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,50 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.view.forEach
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text.toString()
+
+            val textView = TextView(this)
+            textView.text = text
+            binding.contentLayout.addView(textView)
+            binding.inputEditText.setText("")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val texts = ArrayList<String>()
+        binding.contentLayout.forEach {
+            (it as? TextView)?.let { view -> texts.add(view.text.toString()) }
+        }
+        outState.putStringArrayList(KEY, texts)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.getStringArrayList(KEY)?.let { list ->
+            list.forEach { text ->
+                val textView = TextView(this)
+                textView.text = text
+                binding.contentLayout.addView(textView)
+            }
+        }
+    }
+
+    companion object {
+        private const val KEY = "contentViewKey"
     }
 }
